194dfee5ce5862d1d4ecfc6598d856c79620285f
Craig Dennis <cdennis@twilio.com>
2019-10-26 14:52:15 -0700

    Prettiered

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-
 // This is a single page application and it's all rendered in public/index.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 // Parse the body of requests automatically
 app.use(bodyParser.json());
 
-app.get('/api/messages', async (req, res) => {
-    // TODO: Get a list of messages sent from a specific number
-    const sentMessages = [];
-    // TODO: Gather only the body of those messages for sending to the client
-    const messageBodies = [];
-    res.json(messageBodies);
+app.get("/api/messages", async (req, res) => {
+  // TODO: Get a list of messages sent from a specific number
+  const sentMessages = [];
+  // TODO: Gather only the body of those messages for sending to the client
+  const messageBodies = [];
+  res.json(messageBodies);
 });
 
-app.post('/api/messages', async (req, res) => {
-    const to = req.body.to;
-    const from = process.env.TWILIO_PHONE_NUMBER;
-    const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.compliment}. See more compliments at ${req.headers['referer']}`;
-    // TODO: Send a message
-    res.json({success: false});
+app.post("/api/messages", async (req, res) => {
+  const to = req.body.to;
+  const from = process.env.TWILIO_PHONE_NUMBER;
+  const body = `${req.body.sender} says: ${req.body.receiver} is ${
+    req.body.compliment
+  }. See more compliments at ${req.headers["referer"]}`;
+  // TODO: Send a message
+  res.json({ success: false });
 });
 
 app.listen(port, () => console.log(`Prototype is listening on port ${port}!`));
-
-
