42ed469b7a039511907190e933381f1a0bfd8d78
Craig Dennis <cdennis@twilio.com>
2019-10-26 14:49:59 -0700

    Removes Twilio for rebuilding

--- app.js
+++ app.js
@@ -1,40 +1,31 @@
-// Load the 
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const Twilio = require('twilio');
 const app = express();
 const port = 3000;
 
-// Uses the environment variables set in .env
-const client = new Twilio();
 
 // This is a single page application and it's all rendered in public/index.html
 app.use(express.static('public'));
+// Parse the body of requests automatically
 app.use(bodyParser.json());
 
 app.get('/api/messages', async (req, res) => {
-    const sentMessages  = await client.messages.list({
-        from: process.env.TWILIO_PHONE_NUMBER
-    });
-    const messages = sentMessages.map(sent => sent.body);
-    res.json(messages);
+    // TODO: Get a list of messages sent from a specific number
+    const sentMessages = [];
+    // TODO: Gather only the body of those messages for sending to the client
+    const messageBodies = [];
+    res.json(messageBodies);
 });
 
 app.post('/api/messages', async (req, res) => {
     const to = req.body.to;
     const from = process.env.TWILIO_PHONE_NUMBER;
     const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.compliment}. See more compliments at ${req.headers['referer']}`;
-    const message = await client.messages.create({
-        to,
-        from,
-        body
-    });
-    res.json({body: message.body});
+    // TODO: Send a message
+    res.json({success: false});
 });
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Prototype is listening on port ${port}!`));
 
 
