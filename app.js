3001d7fb3b8b3e7eb6b81d7ec424f16bb2fb78cd
Glitch (007-complimentr) <none>
2020-10-16 19:59:52 +0000

    🚖🐴 Updates - app.post(/api/compliments) to send a message.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const TwilioClient = require("twilio");
 const app = express();
 const port = 3000;
+
+const client = new TwilioClient();
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
 // This is a single page application and it's all rendered in public/index.html
 app.use(express.static("public"));
@@ -11,9 +15,9 @@ app.use(bodyParser.json());
 
 app.get("/api/compliments", async (req, res) => {
   // Get a list of messages sent from a specific number
-  const sentMessages = [];
-  // TODO: Gather only the body of those messages for sending to the client
-  const compliments = [];
+  const sentMessages = await client.messages.list({from: twilioPhoneNumber});
+  // Gather only the body of those messages for sending to the client
+  const compliments = sentMessages.map(message => message.body);
   res.json(compliments);
 });
 
@@ -21,7 +25,8 @@ app.post("/api/compliments", async (req, res) => {
   const to = req.body.to;
   const from = process.env.TWILIO_PHONE_NUMBER;
   const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.compliment}. See more compliments at ${req.headers.referer}`;
-  // TODO: Send a message
+  // Send a message
+  await client.messages.create({to, from, body});
   res.json({ success: false });
 });
 
